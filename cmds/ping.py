93cf4a2c178aa28f8eb2255ae164fe233674000d
侑介 <96770834+69you@users.noreply.github.com>
2022-05-20 23:58:14 +0800

    Create ping.py

---
+++ cmds/ping.py
@@ -0,0 +1,11 @@
+import discord
+from discord.ext import commands
+from core.classes import cog_all
+
+class ping(cog_all):
+    @commands.command()
+    async def ping(self,ctx):
+        await ctx.send(f'{round(self.bot.latency*1000)} (ms)')
+
+def setup(bot):
+    bot.add_cog(ping(bot))
