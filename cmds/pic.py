bc71973f6729e9173483ecae9c669b92abcd0c98
侑介 <96770834+69you@users.noreply.github.com>
2022-05-20 23:58:11 +0800

    Create pic.py

---
+++ cmds/pic.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+import json
+import random
+from core.classes import cog_all
+
+with open ('setting.json',mode='r',encoding='utf8') as jfile:
+    jdata=json.load(jfile)
+
+class pic(cog_all):
+    @commands.command()
+    async def pic(self,ctx):
+        ramdon_pic=random.choice(jdata['pic'])
+        await ctx.send(ramdon_pic)
+
+def setup(bot):
+    bot.add_cog(pic(bot))
