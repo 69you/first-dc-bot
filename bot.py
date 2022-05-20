d53df3ab76288490342b0038f286faf58d714721
侑介 <96770834+69you@users.noreply.github.com>
2022-05-20 15:01:27 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 from lib2to3.pgen2 import token
+from ntpath import join
 import discord
 from discord.ext import commands
 
@@ -10,5 +11,15 @@ token="OTc3MDg2MDU2OTY3MDUzMzUz.Gk_nmf.V7bpZIwE94TbKIOW644HRgFSWO3vMLsHTf-7EQ"
 
 async def on_ready():
     print(">> Bot is ready")
+
+@bot.event 
+
+async def on_member_join(member):
+    print(f'{member}join')
+
+@bot.event 
+
+async def on_member_remove(member):
+    print(f'{member}leave')
 
 bot.run(token)    
