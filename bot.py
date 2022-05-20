76cfa21bc86232a08015508df4f37aa9f75a3be1
侑介 <96770834+69you@users.noreply.github.com>
2022-05-20 15:32:52 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,13 @@
 from lib2to3.pgen2 import token
 from ntpath import join
+from operator import truediv
 import discord
 from discord.ext import commands
+intents=discord.Intents.default()
 
-bot=commands.Bot(command_prefix="@@")
+
+
+bot=commands.Bot(command_prefix="@@",intents=intents)
 
 token="OTc3MDg2MDU2OTY3MDUzMzUz.Gk_nmf.V7bpZIwE94TbKIOW644HRgFSWO3vMLsHTf-7EQ"
 
