35c5263fe6ce67491636b97747b775af8a8c9b5e
侑介 <96770834+69you@users.noreply.github.com>
2022-05-20 16:49:32 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,27 +4,23 @@ from operator import truediv
 from tabnanny import check
 import discord
 from discord.ext import commands
+
 intents=discord.Intents.all()
-
 bot=commands.Bot(command_prefix="@@",intents=intents)
-
 token="OTc3MDg2MDU2OTY3MDUzMzUz.Gk_nmf.V7bpZIwE94TbKIOW644HRgFSWO3vMLsHTf-7EQ"
 
 @bot.event
-
 async def on_ready():
     channel=bot.get_channel(977121281361182750)
     await channel.send( 'Ready :white_check_mark:' )
 
 
 @bot.event 
-
 async def on_member_join(member):
     channel=bot.get_channel(977121281361182750)
     await channel.send(f'{member} has join')
 
 @bot.event 
-
 async def on_member_remove(member):
     channel=bot.get_channel(977121281361182750)
     await channel.send(f'{member} has leave')
