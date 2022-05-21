1b32b40633dddf6100f3f7b80a153bbe920f5038
侑介 <96770834+69you@users.noreply.github.com>
2022-05-21 16:59:27 +0800

    Update ping.py

--- cmds/ping.py
+++ cmds/ping.py
@@ -5,7 +5,7 @@ from core.classes import cog_all
 class ping(cog_all):
     @commands.command()
     async def ping(self,ctx):
-        await ctx.send(f'{round(self.bot.latency*1000)} (ms)')
+        await ctx.send("延遲 "+f'{round(self.bot.latency*1000)} (ms)')
 
 def setup(bot):
     bot.add_cog(ping(bot))
