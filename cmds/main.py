e8e0c518793f1dc16efa4328de1aac8c02a3a298
侑介 <96770834+69you@users.noreply.github.com>
2022-05-21 23:12:00 +0800

    Update main.py

--- cmds/main.py
+++ cmds/main.py
@@ -1,6 +1,22 @@
 import discord
 from discord.ext import commands
 from core.classes import cog_all
+import json
+
+with open ('setting.json',mode='r',encoding='utf8') as jfile:
+    jdata=json.load(jfile)
+
+
+class backevent(cog_all):
+    @commands.Cog.listener() 
+    async def on_member_join(self,member):
+        channel=self.bot.get_channel(977121281361182750)
+        await channel.send(f'{member} has join')
+
+    @commands.Cog.listener() 
+    async def on_member_remove(self,member):
+        channel=self.bot.get_channel(977121281361182750)
+        await channel.send(f'{member} has leave ')
 
 class load(cog_all):
     @commands.command()
@@ -21,6 +37,7 @@ class reload(cog_all):
         await ctx.send(f'reloaded {extension}')
 
 def setup(bot):
+    bot.add_cog(backevent(bot))
     bot.add_cog(load(bot))
     bot.add_cog(unload(bot))
     bot.add_cog(reload(bot))
