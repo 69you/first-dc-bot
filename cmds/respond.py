1da8d0ceab66ad8d62dcb27f48ad92cc92ed9671
侑介 <96770834+69you@users.noreply.github.com>
2022-05-23 00:20:37 +0800

    Update respond.py

--- cmds/respond.py
+++ cmds/respond.py
@@ -6,62 +6,64 @@ import discord
 from discord.ext import commands
 from core.classes import cog_all
 
-import json
-import random
-with open ('setting.json',mode='r',encoding='utf8') as jfile:
-    jdata=json.load(jfile)
-
 
 class respond(cog_all):
     @commands.Cog.listener()
     async def on_message(self,msg):
 
-        keyword1=["早安"]
+        keyword1=["早安","早昂"]
         for key1 in keyword1:
-            if key1 in msg.content and msg.author != self.bot.user and msg.author.id !=338222603829510164:
+            if key1 in msg.content and msg.author != self.bot.user:
                 await msg.reply("早安")
                 await msg.channel.send("<a:emoji_33:971218065259913226>")
                 break
 
-        keyword2=["午安"]
-        if msg.content in keyword2 and msg.author != self.bot.user:
-            await msg.reply("午安 <:kohane:969515581353123911>")
+        keyword2=["午安"," 午安阿"]
+        for key2 in keyword2:
+            if key2 in msg.content and msg.author != self.bot.user:
+                await msg.reply("午安 <:kohane:969515581353123911>")
+                break
 
         keyword3=["晚安","睡覺","睡覺去"]
-        for key3 in keyword2:
-            if msg.content in keyword3 and msg.author != self.bot.user:
+        for key3 in keyword3:
+            if key3 in msg.content and msg.author != self.bot.user:
                 await msg.reply("晚安")
                 await msg.channel.send("<a:778203433701474364:977583066920931338>") 
                 break  
 
         keyword4=["走開"]
-        if msg.content in keyword4 and msg.author != self.bot.user:
-            await msg.reply("你怎麼叫我走開QQ，不跟你好了啦")
-            await msg.channel.send("<:emoji_46:935534306841997333>")  
+        for key4 in keyword4:
+            if key4 in msg.content and msg.author != self.bot.user:
+                await msg.reply("你怎麼叫我走開QQ，不跟你好了啦")
+                await msg.channel.send("<:emoji_46:935534306841997333>")  
 
         keyword5=["簽","ㄑ"]
         for key5 in keyword5:
-            if msg.content in keyword5 and msg.author != self.bot.user:
+            if key5 in msg.content and msg.author != self.bot.user:
                 await msg.add_reaction("✅")
                 break
 
         keyword6=["❓","?","？"]
         for key6 in keyword6:
-            if msg.content in keyword6 and msg.author != self.bot.user:
+            if key6 in msg.content and msg.author != self.bot.user:
                 await msg.add_reaction("❓")
                 break
 
         keyword7=["<:fu:969506818160209920>"]
-        if msg.content in keyword7 and msg.author != self.bot.user:
-            await msg.add_reaction("🇨")
-            await msg.add_reaction("🇦")
-            await msg.add_reaction("🇱")
-            await msg.add_reaction("🇲")
+        for key7 in keyword7:
+            if key7 in msg.content and msg.author != self.bot.user:
+                await msg.add_reaction("🇨")
+                await msg.add_reaction("🇦")
+                await msg.add_reaction("🇱")
+                await msg.add_reaction("🇲")
+
+        keyword8=["佬","大佬"]
+        for key8 in keyword8:
+            if key8 in msg.content and msg.author != self.bot.user:
+                await msg.channel.send("<a:emoji_14:962669697881477279>")
+                await msg.add_reaction("<a:emoji_14:962669697881477279>")
+                break
         
-        if msg.content =="<@584677291318312963>" :
-            ramdon_word=random.choice(jdata['word'])
-            await msg.channel.send("<@584677291318312963>")
-            await msg.channel.send(ramdon_word)
 
 def setup(bot):
     bot.add_cog(respond(bot))
