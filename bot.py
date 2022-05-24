5096d1c2bef137b0b51ab696d4481e15a2484b4f
侑介 <96770834+69you@users.noreply.github.com>
2022-05-24 23:52:08 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,27 +10,21 @@ import json
 import random
 import os
 import datetime
+import asyncio
 from discord.ext import commands
 
 with open ('setting.json',mode='r',encoding='utf8') as jfile:
     jdata=json.load(jfile)
 
 intents=discord.Intents.all()
-bot=commands.Bot(command_prefix="*",intents=intents)
+bot=commands.Bot(command_prefix="*",intents=intents,help_command=None)
 datetime.timezone
 
 @bot.event
 async def on_ready():
     print("Bot is ready")
     channel=bot.get_channel(977121281361182750)
-    await channel.send("Ready <a:emoji_33:971218065259913226>" )
-
-@bot.command()
-async def help1(ctx):
-    embed=discord.Embed(title="侑介のbot", url="http://yt1.piee.pw/46vhku", description="由侑介#4644所開發的人工智障", color=0xfd12ca, timestamp=datetime.datetime.utcnow())
-    embed.set_author(name="<==這是侑介", icon_url="https://cdn.discordapp.com/avatars/878830839822176287/e947993f71d34bd423b0a24e166ccf42.png?size=4096")
-    embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/977086056967053353/ebc17adf1bc26fe27571586430b56ae8.png?size=4096")
-    await ctx.send(embed=embed) 
+    await channel.send("Ready <a:emoji_33:971218065259913226>" ) 
     
 
 for filename in os.listdir('./cmds'):
@@ -39,4 +33,4 @@ for filename in os.listdir('./cmds'):
 
 
 if __name__=='__main__':
-    bot.run(jdata['token'])    
+    bot.run(jdata['token'])  
