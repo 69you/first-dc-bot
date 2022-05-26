e54dcc5bed7b67ce5296d68913b82da6a516c085
侑介 <96770834+69you@users.noreply.github.com>
2022-05-26 13:47:11 +0800

    Update time.py

--- cmds/time.py
+++ cmds/time.py
@@ -7,6 +7,7 @@ from core.classes import cog_all
 class timereport(cog_all):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
         async def onlinetime():
             await self.bot.wait_until_ready()
             self.channel=self.bot.get_channel(977121281361182750)
@@ -16,6 +17,17 @@ class timereport(cog_all):
                 onlinetime+=900/60
                 await asyncio.sleep(900) #sec
         self.testloop=self.bot.loop.create_task(onlinetime())
+
+        async def test():
+            await self.bot.wait_until_ready()
+            self.channel=self.bot.get_channel(977121281361182750)
+            onlinetime=0
+            while not self.bot.is_closed():
+                await self.channel.send(f"沒忘記我吧,我已經上線了{onlinetime}分鐘")
+                onlinetime+=900/60
+                await asyncio.sleep(900) #sec
+        self.testloop=self.bot.loop.create_task(test())        
+    
     #for log online channel
     @commands.command()
     async def set_channel(self,ctx,channel:int):
@@ -25,6 +37,7 @@ class timereport(cog_all):
         else:
             await ctx.send("你以為你是 `侑介#4644` 嗎")
             await ctx.send("<a:691225128549482496:978100671020679168>")
+
 
 
 def setup(bot):
