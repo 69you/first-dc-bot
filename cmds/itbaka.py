c08e3201757c8c5e4afe7911d17b6da39a200845
侑介 <96770834+69you@users.noreply.github.com>
2022-06-12 14:37:49 +0800

    Update itbaka.py

--- cmds/itbaka.py
+++ cmds/itbaka.py
@@ -6,13 +6,19 @@ import datetime
 class baka(cog_all):
     @commands.command()
     async def it載點(self,ctx):
-        embed=discord.Embed(title="IT版載點", url="http://shorturl.at/jvMS8", description="自己下載吧",  color=0xfd12ca, timestamp=datetime.datetime.utcnow())
+        embed=discord.Embed(title="IT版載點", url="https://drive.google.com/file/d/1UbujzGnivrAS47MvjNg7S-K6w_BvRr8v/view?usp=sharing", description="自己下載吧",  color=0xfd12ca, timestamp=datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=+8))))
         await ctx.send(embed=embed)
 
     @commands.command()
     async def IT載點(self,ctx):
-        embed=discord.Embed(title="IT版載點", url="http://shorturl.at/jvMS8", description="自己下載吧",  color=0xfd12ca, timestamp=datetime.datetime.utcnow())
+        embed=discord.Embed(title="IT版載點", url="https://drive.google.com/file/d/1UbujzGnivrAS47MvjNg7S-K6w_BvRr8v/view?usp=sharing", description="自己下載吧",  color=0xfd12ca, timestamp=datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=+8))))
         await ctx.send(embed=embed)
+
+    @commands.command()
+    async def tbd載點(self,ctx):
+        embed=discord.Embed(title="tbd載點", url="https://drive.google.com/file/d/1UbujzGnivrAS47MvjNg7S-K6w_BvRr8v/view?usp=sharing", description="自己下載吧",  color=0xfd12ca, timestamp=datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=+8))))
+        await ctx.send(embed=embed)
+
 
 def setup(bot):
     bot.add_cog(baka(bot))        
