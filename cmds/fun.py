69003fa6b28475a87c2f845a16503716e6b511b0
侑介 <96770834+69you@users.noreply.github.com>
2022-06-20 22:10:31 +0800

    Update fun.py

--- cmds/fun.py
+++ cmds/fun.py
@@ -63,6 +63,11 @@ class fun(cog_all):
         await asyncio.sleep(1)
         await message.edit(content="延遲 "+f'{round(self.bot.latency*1000)} (ms)')
 
+    @commands.command()
+    async def invite(self,ctx):
+        embed=discord.Embed(title="人工智障的邀請函",url="https://discord.com/api/oauth2/authorize?client_id=977086056967053353&permissions=8&scope=bot",description=f"邀請前請先找 `侑介#4644`", color=0xfd12ca, timestamp=datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=+8))))
+        await ctx.send(embed=embed)
+        
 
 def setup(bot):
   bot.add_cog(fun(bot))
