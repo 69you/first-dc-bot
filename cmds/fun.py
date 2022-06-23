014cd97b0eceabe5691b98a721f273cde2286edc
侑介 <96770834+69you@users.noreply.github.com>
2022-06-23 20:45:46 +0800

    Update fun.py

--- cmds/fun.py
+++ cmds/fun.py
@@ -93,6 +93,7 @@ class fun(cog_all):
         embed=discord.Embed(title="人工智障的邀請函",url="https://discord.com/api/oauth2/authorize?client_id=977086056967053353&permissions=8&scope=bot",description=f"邀請前請先找 `侑介#4644`", color=0xfd12ca, timestamp=datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=+8))))
         await ctx.send(embed=embed)
 
+<<<<<<< HEAD
     @commands.command()
     async def nick(self,ctx, member:discord.Member, nick=None):
         if nick==None:
@@ -113,6 +114,9 @@ class fun(cog_all):
                 await member.edit(nick=nick)
                 embed=discord.Embed(title=f"{member} 的暱稱已經更改",description=f"前: `{name}` ,後: `{nick}` ", color=0xfd12ca, timestamp=datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=+8))))
                 await ctx.send(embed=embed)       
+=======
+        
+>>>>>>> 14a569a5c60c45e3f512c67b40bc9dd54bd15248
 
 def setup(bot):
   bot.add_cog(fun(bot))
