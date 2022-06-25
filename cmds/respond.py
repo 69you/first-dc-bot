6231bde55efbcf642db9f2cf0f2c62239c7dd9d9
侑介 <96770834+69you@users.noreply.github.com>
2022-06-26 03:27:45 +0800

    Update respond.py

--- cmds/respond.py
+++ cmds/respond.py
@@ -14,8 +14,7 @@ class respond(cog_all):
         keyword1=["早安","早昂"]
         for key1 in keyword1:
             if key1 in msg.content and msg.author != self.bot.user:
-                await msg.reply("早安")
-                await msg.channel.send("<a:emoji_33:971218065259913226>")
+                await msg.reply("早安 <a:emoji_33:971218065259913226>")
                 break
 
         keyword2=["午安"," 午安阿"]
@@ -27,13 +26,11 @@ class respond(cog_all):
         keyword3=["晚安","睡覺","睡覺去","<:sleep:969505662289735750>"]
         for key3 in keyword3:
             if key3 in msg.content and msg.author != self.bot.user:
-                await msg.reply("晚安")
-                await msg.channel.send("<a:778203433701474364:977583066920931338>") 
+                await msg.reply("晚安 <a:778203433701474364:977583066920931338>")
                 break  
 
         if msg.content == "走開" and msg.author != self.bot.user:
-            await msg.reply("你怎麼叫我走開QQ，不跟你好了啦")
-            await msg.channel.send("<:emoji_46:935534306841997333>")  
+            await msg.reply("你怎麼叫我走開QQ，不跟你好了啦 <:emoji_46:935534306841997333")
 
         if msg.author.id == (int(jdata["yuusuke id"])) or msg.author.id == (int(jdata["小飄 id"])) or msg.author.id == (int(jdata["小小飄 id"])) or msg.author.id == (int(jdata["煋夜 id"])) or msg.author.id == (int(jdata["死神 id"])) or msg.author.id == (int(jdata["小魚 id"]))  or msg.author.id == (int(jdata["白日夢 id"])):
             if msg.content == "簽" and msg.author != self.bot.user:
@@ -75,39 +72,40 @@ class respond(cog_all):
         keyword12=["E","e"]
         for key12 in keyword12:
             if msg.content == key12 and msg.author != self.bot.user and msg.author.id != (int(jdata["yuusuke id"])):
-                await msg.reply(" ||請問你在工三小?|| ")
-                await msg.channel.send(jdata["ars hit"])
+                await msg.reply("||請問你在工三小?||"+jdata["ars hit"])
                 break
 
         if msg.content=="幹":
               await msg.reply("請停止你的行為 "+ jdata["ars hit"])
 
-        if "<@878830839822176287>" in msg.content and msg.author.guild.id== 935165737314685009:
-          await msg.reply("那個智障正在淺水喔，等他浮上來吧")
-
         keyword9=["假","ru83"]
-        for key9 in keyword9:
-            if key9 in msg.content and msg.author != self.bot.user:
-                await msg.add_reaction("<:u_:978299522172211200>")
-                break
+        if msg.guild.id==938012107167920138:
+            for key9 in keyword9:
+                if key9 in msg.content and msg.author != self.bot.user:
+                    await msg.add_reaction("<:u_:978299522172211200>")
+                    break
 
         keyword10=["您","sup6"]
-        for key10 in keyword10:
-            if key10 in msg.content and msg.author != self.bot.user:
-                await msg.add_reaction("<:fake:978299535128420432>")
-                break
+        if msg.guild.id==938012107167920138:
+            for key10 in keyword10:
+                if key10 in msg.content and msg.author != self.bot.user:
+                    await msg.add_reaction("<:fake:978299535128420432>")
+                    break
 
         keyword11=["您假","sup6ru83"]
-        for key11 in keyword11:
-            if key11 in msg.content and msg.author != self.bot.user:
-                await msg.add_reaction("<:u_:978299522172211200>")
-                await msg.add_reaction("<:fake:978299535128420432>")
-                break
+        if msg.guild.id==938012107167920138:        
+            for key11 in keyword11:
+                if key11 in msg.content and msg.author != self.bot.user:
+                    await msg.add_reaction("<:u_:978299522172211200>")
+                    await msg.add_reaction("<:fake:978299535128420432>")
+                    break
+
         keyword12=["群假","群甲"]
-        for key12 in keyword12:
-            if key12 in msg.content and msg.author!= self.bot.user:
-                await msg.reply("除我")
-                break
+        if msg.guild.id==938012107167920138:
+            for key12 in keyword12:
+                if key12 in msg.content and msg.author!= self.bot.user:
+                    await msg.reply("除我")
+                    break
 
 
 def setup(bot):
