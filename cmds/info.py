acc79cd61b75fb1a431bbed35405b6a32ff0886f
侑介 <96770834+69you@users.noreply.github.com>
2022-06-26 03:27:32 +0800

    Update info.py

--- cmds/info.py
+++ cmds/info.py
@@ -1,4 +1,3 @@
-from ast import alias
 import discord
 from discord.ext import commands
 import asyncio,datetime,json
@@ -9,11 +8,44 @@ class info(cog_all):
     async def severinfo(self,ctx):
         guild=ctx.guild
         #await ctx.send(f"{guild.banner_url}")
-        await ctx.send(f"{guild.owner}")
-        await ctx.send(f"{guild.created_at}")
-        ch=(guild.channels)
-        for i in (len(ch)):
-            print (ch[i])
+        #await ctx.send(f"`伺服器名稱:` {guild.name}")
+        #await ctx.send(f"`伺服器擁有人:`　{guild.owner}")
+        #await ctx.send(f"`伺服器創建日期：`　{guild.created_at}")
+        cha=0
+        for i in guild.text_channels:
+            cha+=1
+        #await ctx.send(f"`文字頻道數量:` {cha}")
+        vcha=0
+        for i in guild.voice_channels:
+            vcha+=1
+        #await ctx.send(f"`語音頻道數量:` {vcha}")
+        emoji=0
+        for i in guild.emojis:
+            emoji+=1
+        #await ctx.send(f"`表情符號數量:` {emoji}")
+        #await ctx.send(str(guild.icon_url))
+        role=0
+        for i in guild.roles:
+            role+=1
+        #await ctx.send(f"`身分組數量:` {role}")
+        #await ctx.send(bool(guild.large))
+        member=0
+        for i in guild.members:
+            if i.bot==False:
+                member+=1
+        #await ctx.send(f"`伺服器人數:` {member}")
+        bot=0
+        for i in guild.members:
+            if i.bot==True:
+                bot+=1
+        #await ctx.send(f"`機器人數量:` {member}")
+        #await ctx.send(guild.splash_url)邀請背景
+        #await ctx.send(f"`加成等級:` {guild.premium_tier}")
+        #await ctx.send(f"`加成數量:` {guild.premium_subscription_count}")
+        boost=""
+        for i in guild.premium_subscribers:
+            boost+="`"+i.name+"#"+i.discriminator+"` "
+        #await ctx.send(f"`加成人員:` {boost}")
 
 
 def setup(bot):
