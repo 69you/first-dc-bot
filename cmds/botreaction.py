57320321158e0e94258b1e60293de16d7ec8dd9c
侑介 <96770834+69you@users.noreply.github.com>
2022-07-04 18:27:40 +0800

    Update botreaction.py

--- cmds/botreaction.py
+++ cmds/botreaction.py
@@ -113,20 +113,7 @@ class botreaction(cog_all):
                     await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"{name}"),status=discord.Status.online)
                     embed=discord.Embed(title="✅ Changing Status Complted!",description=" ",color=0xfd12ca)
                     await ctx.send(embed=embed)
-                    
-    @commands.command()
-    async def restart(self,ctx):
-        def restart_bot(): 
-            os.execv(sys.executable, ['python'] + sys.argv)
-        if ctx.author.id==878830839822176287:
-            a=await ctx.send("<a:715586639623356438:989524987696254996> Restarting Bot...")
-            await restart_bot()
-            embed=discord.Embed(title="✅ Bot Has Been Restart",description=" ",color=0xfd12ca,timestamp=datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=+8))))
-            await a.reply(embed=embed)    
-
-
               
-
 
 def setup(bot):
     bot.add_cog(botreaction(bot))
