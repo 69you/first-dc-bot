fcac3a97a72135c436b2b6d42658eb599890cfdf
侑介 <96770834+69you@users.noreply.github.com>
2022-07-04 18:27:28 +0800

    Update info.py

--- cmds/info.py
+++ cmds/info.py
@@ -4,10 +4,14 @@ import asyncio,datetime,json
 from core.classes import cog_all
 
 class info(cog_all):
-    @commands.command(aliases=["sf"])
+    @commands.command(aliases=["sinfo"])
     async def severinfo(self,ctx):
         guild=ctx.guild
-        embed=discord.Embed(title="伺服器訊息", description=" ", color=0xfd12ca)
+        if guild.banner_url=="":
+            embed=discord.Embed(title="伺服器訊息", description=" ", color=0xfd12ca)
+        else:
+            banner=guild.banner_url_as(format="png")
+            embed=discord.Embed(title="伺服器訊息",url=f"{banner}", description=" ", color=0xfd12ca)
         embed.set_author(name="人工智障取得的資料", icon_url="https://cdn.discordapp.com/emojis/990679568153190430.gif?size=4096")
         icon=guild.icon_url_as(format="png")
         embed.set_thumbnail(url=f"{icon}")
@@ -52,7 +56,7 @@ class info(cog_all):
         embed.add_field(name="`伺服器加成數量`", value=f"{guild.premium_subscription_count}", inline=True)
         boost=""
         for i in guild.premium_subscribers:
-            boost+="`"+i.name+"#"+i.discriminator+"` "   
+            boost+=i.mention+"** **" 
         if boost=="":     
             embed.add_field(name="`伺服器加成人員`", value=f"無", inline=True)
         else:
@@ -71,6 +75,75 @@ class info(cog_all):
         time=datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=+8))).strftime("%Y /%m /%d  %H :%M :%S")
         embed.set_footer(text=f"資料取得時間: {time}")
         await ctx.send(embed=embed)
+    
+    @commands.command(aliases=["uinfo"])
+    async def userinfo(self,ctx,member=None):
+        if member==None:
+            user=ctx.author
+            embed=discord.Embed(title="使用者訊息", description=" ", color=0xfd12ca)
+            embed.set_author(name="人工智障取得的資料", icon_url="https://cdn.discordapp.com/emojis/990679568153190430.gif?size=4096")  
+            icon=user.avatar_url_as(format="png")
+            embed.set_thumbnail(url=f"{icon}")   
+            embed.add_field(name="`使用者名稱`",value=f"{user.name}#{user.discriminator}",inline=True)
+            if user.nick==None:
+                embed.add_field(name="`伺服器暱稱`",value="無設定暱稱",inline=True)
+            else:
+                embed.add_field(name="`伺服器暱稱`",value=f"{user.nick}",inline=True)
+            embed.add_field(name="`使用者id`",value=f"{user.id}",inline=True)   
+            embed.add_field(name="`使用者帳號色彩`",value=f"{user.color}",inline=True)
+            ctime=str(user.created_at)
+            embed.add_field(name="`創建帳號時間`",value=f"{ctime[:-7]}",inline=True)
+            jtime=str(user.joined_at) 
+            embed.add_field(name="`加入伺服器時間`",value=f"{jtime[:-7]}",inline=True)        
+            role=""
+            for i in range(len(user.roles)-1,0,-1):
+                role+=f"{user.roles[i].mention}"+""   
+            embed.add_field(name="`使用者身分組:`",value=f"{role}",inline=True)
+            time=datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=+8))).strftime("%Y /%m /%d  %H :%M :%S")
+            embed.set_footer(text=f"資料取得時間: {time}")
+            await ctx.send(embed=embed)
+        else:
+            user=await ctx.guild.fetch_member(member)
+            embed=discord.Embed(title="使用者訊息", description=" ", color=0xfd12ca)
+            embed.set_author(name="人工智障取得的資料", icon_url="https://cdn.discordapp.com/emojis/990679568153190430.gif?size=4096")  
+            icon=user.avatar_url_as(format="png")
+            embed.set_thumbnail(url=f"{icon}")   
+            embed.add_field(name="`使用者名稱`",value=f"{user.name}#{user.discriminator}",inline=True)
+            if user.nick==None:
+                embed.add_field(name="`伺服器暱稱`",value="無設定暱稱",inline=True)
+            else:
+                embed.add_field(name="`伺服器暱稱`",value=f"{user.nick}",inline=True)
+            embed.add_field(name="`使用者id`",value=f"{user.id}",inline=True)   
+            embed.add_field(name="`使用者帳號色彩`",value=f"{user.color}",inline=True)
+            ctime=str(user.created_at)
+            embed.add_field(name="`創建帳號時間`",value=f"{ctime[:-7]}",inline=True)
+            jtime=str(user.joined_at) 
+            embed.add_field(name="`加入伺服器時間`",value=f"{jtime[:-7]}",inline=True)        
+            role=""
+            for i in range(len(user.roles)-1,0,-1):
+                role+=f"{user.roles[i].mention}"+""   
+            embed.add_field(name="`使用者身分組:`",value=f"{role}",inline=True)
+            time=datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=+8))).strftime("%Y /%m /%d  %H :%M :%S")
+            embed.set_footer(text=f"資料取得時間: {time}")
+            await ctx.send(embed=embed)
+
+    @commands.command()
+    async def aa(self,ctx):
+        online=0
+        idle=0
+        dnd=0
+        offline=0
+        for i in ctx.guild.members:
+            if str(i.status)=="online":
+                online+=1
+            elif str(i.status)=="idle":
+                idle+=1
+            elif str(i.status)=="dnd" or str(i.status)=="do_not_disturb":
+                dnd+=1
+            elif str(i.status)=="offline":
+                offline+=1
+        await ctx.send(f"上線人數:{online}\n勿擾人數:{dnd}\n閒置人數:{idle}\n離線人數:{offline}")
+
 
 
 def setup(bot):
