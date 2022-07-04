0e9b2519952eea04acb5c2bcdf8b13afeaf8ac8c
侑介 <96770834+69you@users.noreply.github.com>
2022-07-04 18:27:52 +0800

    Update fun.py

--- cmds/fun.py
+++ cmds/fun.py
@@ -64,7 +64,7 @@ class fun(cog_all):
             b=await ctx.send("十秒後刪除")
             await asyncio.sleep(10)
             await a.delete()
-            await b.delete()     
+            await b.delete()  
 
     @commands.command(aliases=["be"])
     async def bigemoji(self,ctx,emoji:Union[discord.Emoji, discord.PartialEmoji]):
@@ -87,7 +87,7 @@ class fun(cog_all):
 
     @commands.command()
     async def invite(self,ctx):
-        embed=discord.Embed(title="人工智障的邀請函",url="https://discord.com/api/oauth2/authorize?client_id=977086056967053353&permissions=8&scope=bot",description=f"邀請前請先找 `侑介#4644`", color=0xfd12ca, timestamp=datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=+8))))
+        embed=discord.Embed(title="人工智障的邀請函",url="https://discord.com/api/oauth2/authorize?client_id=977086056967053353&permissions=8&scope=bot%20applications.commands",description=f"邀請前請先找 `侑介#4644`", color=0xfd12ca)
         await ctx.send(embed=embed)
 
     @commands.command()
@@ -114,7 +114,7 @@ class fun(cog_all):
     @commands.command(aliases=["si"])
     async def serverinvite(self,ctx):
             link=await ctx.channel.create_invite()
-            await ctx.send(str(link))
+            await ctx.send(str(link))    
 
 
 def setup(bot):
